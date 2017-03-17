7de7a2d674c686c1294cca0d5fec2227ad06f104
FrozenPhoenix95 <seanstan95@gmail.com>
2017-03-17 15:56:29 -0400

    Updated to restrict responses
    Bot will now only respond to messages in #bot-stuff or #testing-bot. Hardcoded by Channel Id to avoid disruption if the channel's position(s) are moved at any point.

--- BotListener.java
+++ BotListener.java
@@ -7,12 +7,10 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class BotListener extends ListenerAdapter {
 	
 	public void onMessageReceived(MessageReceivedEvent event){
-		if(event.getMessage().getContent().startsWith("!") && event.getMessage().getAuthor().getId() != event.getJDA().getSelfUser().getId()){
-			Main.handleCommand(Main.parser.parse(event.getMessage().getContent().toLowerCase(), event));
+		if(event.getTextChannel().getId().equals("273989023108825090") || event.getTextChannel().getId().equals("291145108953497601")){
+			if(event.getMessage().getContent().startsWith("!") && event.getMessage().getAuthor().getId() != event.getJDA().getSelfUser().getId()){
+				Main.handleCommand(Main.parser.parse(event.getMessage().getContent().toLowerCase(), event));
+			}
 		}
-	}
-	
-	public void onReady(ReadyEvent event){
-		//Main.log("status", "Logged in as: " + event.getJDA().getSelfUser().getName());
 	}
 }
